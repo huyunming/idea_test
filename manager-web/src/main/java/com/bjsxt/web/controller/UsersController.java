8fa95c36875378811f45877c7595124b3b6e2c62
Administrator <abc@163.com>
2019-03-12 10:22:37 +0800

    UsersControler添加Rest Client测试代码

--- manager-web/src/main/java/com/bjsxt/web/controller/UsersController.java
+++ manager-web/src/main/java/com/bjsxt/web/controller/UsersController.java
@@ -4,7 +4,9 @@ import com.bjsxt.pojo.Users;
 import com.bjsxt.service.UsersServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Auther: huyunming
@@ -22,6 +24,30 @@ public class UsersController {
     @RequestMapping("/addUser")
     public String addUser(Users users){
         this.usersServices.addUser(users);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doGet/{userid}", method = RequestMethod.GET)
+    public String doGet(@PathVariable Integer userid){
+        System.out.println("doGet....."+userid);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPost",method = RequestMethod.POST)
+    public String doPost(Users users){
+        System.out.println("doPost......"+users);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doDelete",method = RequestMethod.DELETE)
+    public String doDelete(Integer userid){
+        System.out.println("doDelete......"+userid);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPut",method = RequestMethod.PUT)
+    public String doPut(){
+        System.out.println("doPut......");
         return "ok";
     }
 }
